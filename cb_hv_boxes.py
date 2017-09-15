86379a93636a9b63306110961a074e9851fb9819
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-09-15 11:54:07 +0200

    Add some comments, remove unnecessary sleep

--- cb_hv_boxes.py
+++ cb_hv_boxes.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+This script is used to set the correction values for the CB HV boxes
+The values are read from a file specified in the beginning of the script
+To make the output less verbose, set debug to False
+'''
+
+from sys import exit
 from telnetlib import Telnet
-from time import sleep
 import logging
 
 debug = True
@@ -13,23 +21,25 @@ if debug:
 logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %I:%M:%S', level=level)
 
 def print_telnet(str):
+    """remove the telnet prompt from the string and print the output using logging.DEBUG level"""
     cleaned = str.rstrip(b'\r\n>').decode('ascii').strip()
     if not cleaned:
         cleaned = 'empty'
     logging.debug('Telnet response: ' + cleaned)
 
 logging.debug('Try to read file ' + gains_file)
-gains = open(gains_file, 'r')
-hv_gains = gains.readlines()
-gains.close()
+with open(gains_file, 'r') as gains:
+    hv_gains = gains.readlines()
+if not len(hv_gains):
+    logging.error('No lines read from file %s, please check the provided file' % gains_file)
+    exit(1)
 logging.debug('Successfully read %d lines from file' % len(hv_gains))
 
-logging.debug('start loop')
+logging.debug('start looping over the boxes')
 for i in range(1, 19):
     host = host_prefix % i
     logging.info('Connecting to box ' + host)
     tn = Telnet(host)
-    sleep(1)
     response = tn.read_until(b'>')
     print_telnet(response)
     logging.debug('Send eemem unprotect')
@@ -37,6 +47,8 @@ for i in range(1, 19):
     response = tn.read_until(b'>')
     print_telnet(response)
     first_card = i*5-4
+    tn.close()
+    exit(0)
     # loop over cards per box
     for j in range(5):
         card = first_card + j
@@ -67,13 +79,14 @@ for i in range(1, 19):
         response = tn.read_until(b'>')
         print_telnet(response)
         
+    # finished setting the values for the different channels per card, now store them
     logging.debug('Send eemem protect')
     tn.write(b"eemem protect\r\n")
     response = tn.read_until(b'>')
     print_telnet(response)
     logging.debug('Send read_config')
     tn.write(b"read_config\r\n")
-    response = tn.read_until(b'>', 30)
+    response = tn.read_until(b'>', 40)
     print_telnet(response)
     logging.debug('Send eemem print')
     tn.write(b"eemem print\r\n")
